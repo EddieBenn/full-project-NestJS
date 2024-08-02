e6e9a7144af8e26f9ee1378ef721746abf8c4e45
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-08-02 09:33:10 +0100

    adjustments

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import {
   type NestExpressApplication,
 } from '@nestjs/platform-express';
 import rateLimit from 'express-rate-limit';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';;
 
 async function bootstrap(): Promise<NestExpressApplication> {
   initializeTransactionalContext();
