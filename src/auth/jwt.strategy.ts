e88d35b3fb7d6e37a4ba06238271fcc27f3da49f
Edidiong Ndaobong <edidiong.ndaobong@maxdrive.ai>
2024-08-17 14:27:36 +0100

    jwt fixes

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,9 +1,10 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { IReqUser } from 'src/base.entity';
 import { Request } from 'express';
+import { ExtractJwt } from 'passport-jwt';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
